e9028ef09d8ef999922d2035d60c2559f3efd2f1
Dave <macharia3041@gmail.com>
2019-08-06 14:43:26 +0300

    second accordion layer

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { menuOptions } from 'src/app/menuJson';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  menuOptions = menuOptions;
+
   constructor() { }
 
   ngOnInit() {
+    this.menuOptions.forEach(option => console.log(option['name']));
+    const dataProperties = {
+      
+    }
+    this.menuOptions[0]['properties'] = dataProperties;
   }
 
 }
