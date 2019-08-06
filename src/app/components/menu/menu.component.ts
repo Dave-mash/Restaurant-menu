ab1a0e2f18f90550cf69b7a6ab601bd8edca3e79
Dave <macharia3041@gmail.com>
2019-08-07 00:14:45 +0300

    more styling

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -9,6 +9,7 @@ import { menuOptions } from 'src/app/menuJson';
 export class MenuComponent implements OnInit {
 
   menuOptions = menuOptions;
+  relatedOptions = [];
 
   constructor() { }
 
@@ -16,6 +17,30 @@ export class MenuComponent implements OnInit {
     const element = el.textContent.trim();
     el.setAttribute('data-target', `#${element}`)
     console.log(el.getAttribute('data-target'))
+  }
+
+  checkboxToggle(el) {
+    const element = el.parentElement.nextElementSibling;
+
+    if (el.checked) {
+      // this.menuOptions.forEach(option => {
+      //   option.related.forEach(relatedOption => {
+      //     if (el.nextElementSibling.textContent == relatedOption['name']) {
+      //       this.relatedOptions = relatedOption['choices'];
+      //     }
+      //   })
+      // });
+      // console.log(`#${el.nextElementSibling.textContent}`)
+      console.log('checked')
+      console.log(element);
+      element.style.display = 'flex';
+    } else {
+      console.log('unchecked')
+      // this.relatedOptions = [];
+      console.log(element.style.display);
+      // console.log(this.relatedOptions)
+      element.style.display = 'none';
+    }
   }
 
   ngOnInit() {
